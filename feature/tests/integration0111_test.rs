d51e9d8703a9a5e9d2c70563d904ee8b1f53a797
xueyanli <xueyanli19950914@gmail.com>
2023-07-31 20:58:05 +0800

    update 0111 mock rpc

--- feature/tests/integration0111_test.rs
+++ feature/tests/integration0111_test.rs
@@ -8,7 +8,7 @@ mod common;
 
 
 #[test]
-// #[ignore]
+#[ignore]
 fn test_estimate_cycles() {
     // 获取方法名和参数
     let method = "estimate_cycles";
@@ -43,22 +43,49 @@ fn test_estimate_cycles() {
 }
 
 #[test]
+#[ignore]
 fn test_get_block() {
     // 获取方法名和参数
     let method = "get_block";
     let params = "[block_hash]";
+    let params2 = "data2";
+    // 调用 mock_rpc 函数获取结果
+    let result = mockrpc::get_mock_test_data(method, params).unwrap();
+    // let result2 = mockrpc::get_mock_test_data(method, params2).unwrap();
+    let mut ckb_client = CkbRpcClient::new(&*result.url);
+    // let mut ckb_client2 = CkbRpcClient::new(&*result2.url);
+    const CODE_HASH: H256 = h256!("0xa5f5c85987a15de25661e5a214f2c1449cd803f071acc7999820f25246471f40");
+    let block = ckb_client.get_block(CODE_HASH).unwrap().unwrap();
+    // let block2 = ckb_client2.get_block(CODE_HASH).unwrap().unwrap();
+    println!("{:#?}", block);
+    // println!("{:#?}", block2);
+    assert_eq!("0x".to_owned() + &block.header.hash.to_string(), remove_quotes(&serde_json::to_string(
+        &result.response_data["result"]["header"]["hash"]).unwrap()));
+}
+
+#[test]
+#[ignore]
+fn test_get_consensus() {
+    // 获取方法名和参数
+    let method = "get_consensus";
+    let params = "[]";
     // 调用 mock_rpc 函数获取结果
     let result = mockrpc::get_mock_test_data(method, params).unwrap();
     let mut ckb_client = CkbRpcClient::new(&*result.url);
-    let json_data = &result.request_data["params"][0];
-    let json_str = serde_json::to_string(json_data).unwrap();
-    println!("{:#?}", json_str);
-    let code_hash = remove_quotes(&json_str);
-    println!("{:#?}", code_hash);
-    // const CODE_HASH_FAIL: H256 = h256!(code_hash);
-    const CODE_HASH: H256 = h256!("0xa5f5c85987a15de25661e5a214f2c1449cd803f071acc7999820f25246471f40");
-    let block = ckb_client.get_block(CODE_HASH).unwrap().unwrap();
-    println!("{:#?}", block);
-    assert_eq!("0x".to_owned() + &block.header.hash.to_string(), remove_quotes(&serde_json::to_string(
-        &result.response_data["result"]["header"]["hash"]).unwrap()));
+    let consensus = ckb_client.get_consensus();
+    println!("{:#?}", consensus);
+}
+
+#[test]
+// #[ignore]
+fn test_get_deployments_info() {
+    // 获取方法名和参数
+    let method = "get_deployments_info";
+    let params = "[]";
+    // 调用 mock_rpc 函数获取结果
+    let result = mockrpc::get_mock_test_data(method, params).unwrap();
+    println!("{:#?}", result.response_data);
+    let mut ckb_client = CkbRpcClient::new(&*result.url);
+    let deployments_info = ckb_client.get_deployments_info();
+    println!("{:#?}", deployments_info);
 }
