8b4e5dc952badda3a954cd73f3956fb8d765cdf2
gpBlockchain <744158715@qq.com>
2023-10-24 12:05:04 +0800

    fix python

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,4 @@
 git clone -b v0.111.0 https://github.com/gpBlockchain/ckb-rpc-mock-data.git
 cd ckb-rpc-mock-data
 pip install -r requirements.txt
-python3 api/index.py > index.log 2>&1 &
+python api/index.py > index.log 2>&1 &
