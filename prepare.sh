819d62bec1e539eb764e06a0576ff9a1869bed3d
iu小迷弟 <15168316096@163.com>
2024-05-08 11:01:58 +0000

    v116

--- prepare.sh
+++ prepare.sh
@@ -1,11 +1,10 @@
-git clone -b v0.111.0 https://github.com/gpBlockchain/ckb-rpc-mock-data.git
+git clone -b v0.116.0 https://github.com/cryptape/ckb-rpc-mock-data.git
 cd ckb-rpc-mock-data
 pip install -r requirements.txt
 pip install Werkzeug==2.2.2
 python api/index.py > index.log 2>&1 &
 cd ../
-git clone https://github.com/gpBlockchain/ckb-light-client-rpc-mock-data.git
+git clone https://github.com/cryptape/ckb-light-client-rpc-mock-data
 cd ckb-light-client-rpc-mock-data
-git checkout 03526cf054f7e56b88b073ddfe4be9bdc7c8e8c0
 pip install -r requirements.txt
 python api/index.py > index.log 2>&1 &
