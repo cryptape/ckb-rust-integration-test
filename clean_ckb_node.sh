643dfc63e7b9bdfe272b40717ba8c22f42ba9772
gpBlockchain <744158715@qq.com>
2025-06-11 20:15:41 +0800

    add docker

---
+++ clean_ckb_node.sh
@@ -0,0 +1,9 @@
+cd docker
+docker compose down
+rm -rf layer1/ckb/data/ancient
+rm -rf layer1/ckb/data/db
+rm -rf layer1/ckb/data/indexer
+rm -rf layer1/ckb/data/logs
+rm -rf layer1/ckb/data/tmp
+rm -rf layer1/ckb/data/tx_pool
+
